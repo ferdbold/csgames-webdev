2858557e45faec46ac54bdd609d159d5f25db0dd
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-03-14 18:53:45 +0000

    link home page

--- spookyducky/views.py
+++ spookyducky/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.template import loader
 
-# Create your views here.
+def index(request):
+    # template = loader.get_template('spookyducky/index.html')
+    # return HttpResponse(template.render())
+    return render(request, 'spookyducky/index.html')
+    
+def login_user(request):
+    state = "Please log in below..."
+    username = password = ''
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    return render_to_response('login.html',{'state':state, 'username': username})
