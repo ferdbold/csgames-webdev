56bbfdf264686eb9edcc2e40feafc3370076b49c
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-03-14 18:19:53 +0000

    created base app

--- csgames_webdev/urls.py
+++ csgames_webdev/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'csgames_webdev.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^spookyducky/', include('spookyducky.urls', namespace = 'spookyducky')),
     url(r'^admin/', include(admin.site.urls)),
 )
