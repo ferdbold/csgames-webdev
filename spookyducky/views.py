d5461b8bcffd9fc4b23de90d6f194b218bca4ab7
Frédéric Bolduc <bolducfrederic@gmail.com>
2015-03-14 19:17:01 +0000

    Login success

--- spookyducky/views.py
+++ spookyducky/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, render_to_response, HttpResponse
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render, render_to_response, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 from django.template import loader
+from django.core.urlresolvers import reverse
 
 def index(request):
     return render(request, 'spookyducky/index.html')
@@ -21,5 +22,9 @@ def login_user(request):
                 state = "Your account is not active, please contact the site admin."
         else:
             state = "Your username and/or password were incorrect."
+            
+    return render(request, 'spookyducky/login.html', {'state': state, 'username': username})
 
-    return render_to_response('spookyducky/login.html', {'state':state, 'username': username})
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('spookyducky:index'))
